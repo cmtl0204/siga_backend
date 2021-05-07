a8967ebd9e31f6fc66531741ca8d8e6b34be70c7
yogledis <yogledys1981@gmail.com>
2021-05-07 13:12:26 -0500

    se le quito la relación con learning_results_career, ya que ahora es un Json

--- database/migrations/2021_04_20_3_create_portfolio__relation_learning_results_table.php
+++ database/migrations/2021_04_20_3_create_portfolio__relation_learning_results_table.php
@@ -13,8 +13,8 @@ class CreatePortfolioRelationLearningResultsTable extends Migration
             $table->foreignId('learning_result_id')->constrained('portfolio.learning_results')
                 ->comment('FK de la tabla learning_results (resultados de a_rendizaje)');
             //tabla career no esta creada se direccina al ignug
-            $table->foreignId('learning_result_career_id')->constrained('app.career_learning_results')
-                ->comment('FK de la tabla learning_results_career (resultados de a_rendizaje de la carrera)');
+           // $table->foreignId('learning_result_career_id')->constrained('app.career_learning_results')
+             //   ->comment('FK de la tabla learning_results_career (resultados de a_rendizaje de la carrera)');
             $table->foreignId('contribution_id')->constrained('app.catalogues')
                 ->comment('fk de catalogo y posibles valoires (medio, alta,bajo)');
             $table->timestamps();
