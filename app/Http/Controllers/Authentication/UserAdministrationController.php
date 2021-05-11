180af67fa9a53a11045d75d40564b393b2cf8a2a
Pachuka <33724801+Armandot1998@users.noreply.github.com>
2021-05-11 17:20:35 -0500

    finish the methods index and show with sistem

--- app/Http/Controllers/Authentication/UserAdministrationController.php
+++ app/Http/Controllers/Authentication/UserAdministrationController.php
@@ -21,31 +21,21 @@ class  UserAdministrationController extends Controller
 { 
     public function index(Request $request)
     {
-        $rol = $request->input('role');
-        $has_role = false; // Only boolean
-        $system = null; // Roles's System
-        // Search Request Role in The Token User
-        foreach ($request->user()->roles as $u_roles){
-            if ($u_roles->id == $rol) {
-                $has_role = true;
-                $system = $u_roles->system_id;
-                break;
-            }//Else : The Login user don't have te recuest role
-        }
+      $system = $request->input('system');
+      $search = $request->input('search');
 
-        //Search Users If the User Have Role
-        if ($has_role) {
             if ($request->has('search')) {
                 $users = User::whereHas('roles', function($role) use ($system) {
                     $role->where('system_id', '=', $system);
                 })
-                /*
-                ->email($request->input('search'))
-                ->firstlastname($request->input('search'))
-                ->firstname($request->input('search'))
-                ->identification($request->input('search'))
-                ->secondlastname($request->input('search'))
-                ->secondname($request->input('search'))
+                ->where(function($query) use ($search){
+                    $query->email($search);
+                    $query->firstlastname($search);
+                    $query->firstname($search);
+                    $query->identification($search);
+                    $query->secondlastname($search);
+                    $query->secondname($search);
+                    })
                 ->with(['institutions' => function ($institutions) {
                     $institutions->orderBy('name');
                 }])
@@ -56,8 +46,8 @@ class  UserAdministrationController extends Controller
                                 $route->with('module')->with('type')->with('status');
                             }])->with('institution');
                         }]);
-                }])*/
-                ->get();
+                }])
+                 ->paginate($request->input('per_page'));
             }else{
                 $users = User::whereHas('roles', function($role) use ($system) {
                     $role->where('system_id', '=', $system);
@@ -75,8 +65,8 @@ class  UserAdministrationController extends Controller
                 }])
                 ->paginate($request->input('per_page'));
             }
-        }
-        if(sizeof($users)===0){
+        
+        if($users->count()===0){
             return response()->json([
                 'data' => null,
                 'msg' => [
@@ -96,21 +86,7 @@ class  UserAdministrationController extends Controller
 
     public function show($idUser, Request $request)
     {
-        $rol = $request->input('role');
-        $has_role = false; // Only boolean
-        $system = null; // Roles's System
-        $user = null;
-        // Search Request Role in The Token User
-        foreach ($request->user()->roles as $u_roles){
-            if ($u_roles->id == $rol) {
-                $has_role = true;
-                $system = $u_roles->system_id;
-                break;
-            }//Else : The Login user don't have te recuest role
-        }
-
-        //Search Users If the User Have Role
-        if ($has_role) {
+      $system = $request->input('system');
             $user = User::whereHas('roles', function($role) use ($system) {
                     $role->where('system_id', '=', $system);
                 })
@@ -126,7 +102,6 @@ class  UserAdministrationController extends Controller
                 }])
                 ->where('id', $idUser)
                 ->first();
-        }
         if(!$user){
             return response()->json([
                 'data' => null,
@@ -147,36 +122,25 @@ class  UserAdministrationController extends Controller
 
     public function store(Request $request)
     {
-        $catalogues = json_decode(file_get_contents(storage_path() . "/catalogues.json"), true);
-
-        $user = new User();
-        $user->identification = $request->input('identification');
-        $user->username = $request->input('username');
-        $user->first_name = $request->input('first_name');
-        $user->second_name = $request->input('second_name');
-        $user->first_lastname = $request->input('first_lastname');
-        $user->second_lastname = $request->input('second_lastname');
-        $user->birthdate = $request->input('birthdate');
-        $user->email = $request->input('email');
-        $user->password = Hash::make($request->input('password'));
-
-        //$blood_type = $catalogues['catalogue']['blood_type']['o+'];
-        //$civil_status = $catalogues['catalogue']['civil_status']['married'];
-        $ethnicOrigin = $catalogues['catalogue']['ethnic_origin']['mestizo'];
-        //$location = $catalogues['catalogue']['location']['country'];
-        //$identificationType = $catalogues['catalogue']['identification_type']['cc'];
-        //$sex = $catalogues['catalogue']['sex']['male'];
-        //$gender = $catalogues['catalogue']['gender']['male'];
-
-        //$user->bloodType()->associate($blood_type);
-        //$user->civilStatus()->associate($civil_status);
-        $user->ethnicOrigin()->associate($ethnicOrigin);
-        //$user->address()->associate($location);
-        //$user->identificationType()->associate($identificationType);
-        //$user->sex()->associate($sex);
-        //$user->gender()->associate($gender);
-        $user->status()->associate(Status::find($request->input('status')));
-        $user->save();
+        $user = User::where('identification', $request->input('identification'))
+        ->get();
+        if($user->count()===0){
+            $user = new User();
+            $user->identification = $request->input('identification');
+            $user->username = $request->input('username');
+            $user->first_name = $request->input('first_name');
+            $user->second_name = $request->input('second_name');
+            $user->first_lastname = $request->input('first_lastname');
+            $user->second_lastname = $request->input('second_lastname');
+            $user->birthdate = $request->input('birthdate');
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+    
+            $user->status()->associate(Status::getInstance($request->input('status')));
+            $user->save();
+        }else{
+            return 'existe el usuario';
+        }
         return response()->json([
             'data' => $user,
             'msg' => [
