16ad19287dc01a540862f18e62135ca8a2bcc590
Carlos Toapanta <cav.toapanta@yavirac.edu.ec>
2021-05-19 17:39:11 -0500

    modulo

---
+++ app/Models/Cecy/Registration.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Models\Cecy;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as Auditing;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\App\Status;
+use App\Models\App\File;
+use App\Models\App\Image;
+use App\Models\App\Catalogue;
+
+
+/**
+ * @property BigInteger id
+ * @property date date
+ * @property string number
+ */
+
+class Skill extends Model implements Auditable
+{
+    use HasFactory;
+    use Auditing;
+    use SoftDeletes;
+
+    protected static $instance;
+
+    protected $connection = 'pgsql-cecy';
+
+    protected $table = 'cecy.skills';
+
+    protected $fillable = [
+        'date',
+        'number',
+    ];
+
+    protected $casts = [
+        'deleted_at'=>'date:Y-m-d h:m:s',
+        'created_at'=>'date:Y-m-d h:m:s',
+        'updated_at'=>'date:Y-m-d h:m:s',
+    ];
+
+    // Instance
+    public static function getInstance($id)
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static;
+        }
+        static::$instance->id = $id;
+        return static::$instance;
+    }
+
+    // Relationships
+    public function participant()
+    {
+        return $this->belongsTo(Participant::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    // Accessors
+    public function getFullDescriptionAttribute()
+    {
+        return "{$this->attributes['id']}.{$this->attributes['description']}";
+    }
+
+    // Mutators
+    public function setDescriptionAttribute($value)
+    {
+        $this->attributes['description'] = strtoupper($value);
+    }
+
+    // Scopes
+    public function scopeDescription($query, $description)
+    {
+        if ($description) {
+            return $query->where('description', 'ILIKE', "%$description%");
+        }
+    }
+
+}
