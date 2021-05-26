7399f0e34a04a0fc83f07c0e3a9f33b80430abde
marcduk <mec.duque@yavirac.edu.ec>
2021-05-26 01:01:55 -0500

    Creación de modelos y controladores de Sgnature y LEarningResults

--- database/migrations/2021_04_20_2_create_portfolio__learning_results_table.php
+++ database/migrations/2021_04_20_2_create_portfolio__learning_results_table.php
@@ -12,12 +12,17 @@ class CreatePortfolioLearningResultsTable extends Migration
             $table->id();
             $table->foreignId('pea_id')->constrained('portfolio.peas')
                    ->comment('fk de tabla pea, un pea tiene varios resultados de aprendizaje ');
+                   
             $table->foreignId('parent_id')->constrained('portfolio.learning_results')
                    ->comment('campo que hace la recursividad, apunta al id de esta tabla');
+
+
             $table->string('code')->required()
                   ->comment('guada los valores 1, 1.1, 1.1.1 ......etc, este campo no debe permitir nulo y mediante programación se deben auto generar');
             $table->text('description')
                   ->comment('texto de la unidad de competencia, elemento de competencio, resultados de aprendizaje o forma de evidenciar');
+
+
             $table->foreignId('type_id')->constrained('app.catalogues')
                    ->comment('fk de la tabla catalogues, los valores son: UNIDADES DE COMPETENCIA / PROCESOS, ELEMENTOS DE COMPETENCIA / SUB-PROCESOS,RESULTADOS DE APRENDIZAJE /ACTIVIDADES o FORMAS DE EVIDENCIAR A LOS R.D.A.');
             $table->softDeletes();
