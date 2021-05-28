f78c440ab3cc96b7b363102ce9fccd18c842f5be
Mauricio Tamayo <cesar.tamayo0204@gmail.com>
2021-05-28 13:13:50 -0500

    add academic periods

--- database/migrations/2020_06_22_2_create_app__academic_periodables_table.php
+++ database/migrations/2020_06_22_2_create_app__academic_periodables_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAppAcademicPeriodsTable extends Migration
+class CreateAppAcademicPeriodablesTable extends Migration
 {
 
     public function up()
     {
-        Schema::connection('pgsql-app')->create('academic_periods', function (Blueprint $table) {
+        Schema::connection('pgsql-app')->create('academic_periodables', function (Blueprint $table) {
             $table->id();
-            $table->string('code')->unique();
-            $table->string('name');
+            $table->foreignId('academic_period_id')->constrained('app.academic_periods');
+            $table->morphs('academic_periodable');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -20,6 +20,6 @@ class CreateAppAcademicPeriodsTable extends Migration
 
     public function down()
     {
-        Schema::connection('pgsql-app')->dropIfExists('academic_periods');
+        Schema::connection('pgsql-app')->dropIfExists('academic_periodables');
     }
 }
--- database/migrations/2020_06_22_2_create_app__academic_periods_table.php
+++ database/migrations/2020_06_22_2_create_app__academic_periods_table.php
@@ -4,25 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAppSchoolPeriodsTable extends Migration
+class CreateAppAcademicPeriodsTable extends Migration
 {
 
     public function up()
     {
-        Schema::connection('pgsql-app')->create('school_periods', function (Blueprint $table) {
+        Schema::connection('pgsql-app')->create('academic_periods', function (Blueprint $table) {
             $table->id();
-            $table->morphs('school_periodable');
-            $table->foreignId('status_id')->constrained('app.catalogues');
             $table->string('code')->unique();
             $table->string('name');
-            $table->date('start_date');
-            $table->date('end_date')->nullable();
-            $table->date('ordinary_start_date');
-            $table->date('ordinary_end_date');
-            $table->date('extraordinary_start_date');
-            $table->date('extraordinary_end_date');
-            $table->date('especial_start_date');
-            $table->date('especial_end_date');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -30,6 +20,6 @@ class CreateAppSchoolPeriodsTable extends Migration
 
     public function down()
     {
-        Schema::connection('pgsql-app')->dropIfExists('school_periods');
+        Schema::connection('pgsql-app')->dropIfExists('academic_periods');
     }
 }
