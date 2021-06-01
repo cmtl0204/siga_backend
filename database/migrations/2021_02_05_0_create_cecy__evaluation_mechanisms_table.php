d7c6cfd4fc67b9fb4e0081cbdbd48d31f561d8aa
Mauricio Tamayo <cesar.tamayo0204@gmail.com>
2021-06-01 14:43:39 -0500

    add migrations cecy

--- database/migrations/2021_02_05_0_create_cecy__evaluation_mechanisms_table.php
+++ database/migrations/2021_02_05_0_create_cecy__evaluation_mechanisms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCecyEvaluationMechanismsTable extends Migration
+class CreateEvaluationMechanismsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,14 @@ class CreateCecyEvaluationMechanismsTable extends Migration
      */
     public function up()
     {
+        //periodo_lectivo
         Schema::connection('pgsql-cecy')->create('evaluation_mechanisms', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('course_id')
-                ->comment('FK curso')
-                ->constrained('cecy.courses'); //
-            $table->foreignId('type_id')
-                ->comment('evaluacion diagnostica, evaluacion proceso formativo y evaluacion final')
-                ->constrained('app.catalogues');
-            $table->string('technique');
-            $table->string('instrument');
-            $table->softDeletes();
+            $table->foreignId('state_id')->constrained("ignug.states"); //id_estados
+            //$table->foreignId('status_id')->constrained("catalogues"); //id_estatus
+            $table->foreignId('type_id')->constrained('ignug.catalogues'); //evaluacion diagnostica, evaluacion proceso formativo y evaluacion final
+            $table->string('technique',200); //tecnica
+            $table->string('instrument',200); //instrumento
             $table->timestamps();
         });
     }
--- database/migrations/2021_02_05_1_create_cecy__authorities_table_y.php
+++ database/migrations/2021_02_05_1_create_cecy__authorities_table_y.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCecyAuthoritiesTable extends Migration
+class CreateAuthoritiesTable extends Migration
 {
 
     public function up()
@@ -12,15 +12,15 @@ class CreateCecyAuthoritiesTable extends Migration
         Schema::connection('pgsql-ignug')->create('authorities', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('authentication.users')->comment('usuario clave foreanea de Authentication.user ');
-            $table->foreignId('position_id')->constrained('app.catalogues')
-                ->comment('cargo en el cecy, datos como especialista,responsable de cecy,logistica');
-            $table->foreignId('status_id')->nullable()->constrained('ignug.catalogues')
-                ->comment('datos como suspendio o retirado de catologue');
+            $table->foreignId('position_id')->constrained('app.catalogues')->comment('cargo en el cecy, datos como especialista,responsable de cecy,logistica');
+            $table->foreignId('status_id')->nullable()->constrained('ignug.catalogues')->comment('datos como suspendio o retirado de catologue');
             $table->json('functions')->nullable()->comment('Funciones que tiene dentro del Cecy');
             $table->date('start_date')->nullable()->comment('Fecha inicio de la gestión');
             $table->date('end_date')->nullable()->comment('Fecha fin de la gestión');
             $table->softDeletes();
             $table->timestamps();
+
+
         });
     }
 
