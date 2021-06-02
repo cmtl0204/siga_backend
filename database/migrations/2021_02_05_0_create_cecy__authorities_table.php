4905b784e1e77d256ff7686f35c5707c832c585f
jona <jsc.flores@yavirac.edu.ec>
2021-06-01 21:06:36 -0500

    c

--- database/migrations/2021_02_05_0_create_cecy__authorities_table.php
+++ database/migrations/2021_02_05_0_create_cecy__authorities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCecyAuthoritiesTable extends Migration
+class CreateAuthoritiesTable extends Migration
 {
 
     public function up()
@@ -12,15 +12,22 @@ class CreateCecyAuthoritiesTable extends Migration
         Schema::connection('pgsql-cecy')->create('authorities', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('authentication.users')->comment('usuario clave foreanea de Authentication.user ');
+<<<<<<< HEAD:database/migrations/2021_02_05_0_create_cecy__authorities_table.php
             $table->foreignId('position_id')->constrained('app.catalogues')
                 ->comment('cargo en el cecy, datos como especialista,responsable de cecy,logistica');
             $table->foreignId('status_id')->nullable()->constrained('app.catalogues')
                 ->comment('datos como suspendio o retirado de catologue');
+=======
+            $table->foreignId('position_id')->constrained('app.catalogues')->comment('cargo en el cecy, datos como especialista,responsable de cecy,logistica');
+            $table->foreignId('status_id')->nullable()->constrained('ignug.catalogues')->comment('datos como suspendio o retirado de catologue');
+>>>>>>> mod_4_cecy:database/migrations/2021_02_05_1_create_cecy__authorities_table_y.php
             $table->json('functions')->nullable()->comment('Funciones que tiene dentro del Cecy');
             $table->date('start_date')->nullable()->comment('Fecha inicio de la gestión');
             $table->date('end_date')->nullable()->comment('Fecha fin de la gestión');
             $table->softDeletes();
             $table->timestamps();
+
+
         });
     }
 
--- database/migrations/2021_02_05_0_create_cecy__registrations_table.php
+++ database/migrations/2021_02_05_0_create_cecy__registrations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCecyRegistrationsTable extends Migration
+class CreateRegistrationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,20 @@ class CreateCecyRegistrationsTable extends Migration
      */
     public function up()
     {
+        //matriculas
         Schema::connection('pgsql-cecy')->create('registrations', function (Blueprint $table) {
             $table->id();
-            $table->date('date')
-                   ->comment('fecha_matricula');
+            $table->date('date_registration')
+                   ->comment('fecha_matricula'); 
             $table->foreignId('participant_id')->constrained('cecy.participants')
-                  ->comment('Participante a ser matriculados');
-            $table->foreignId('status_id')->constrained('app.status')
-                  ->comment('Estado de la matricula (inscrito, matriculado, anulado, desertor)');
-            $table->foreignId('type_id')->nullable()->constrained('app.catalogues')
-                  ->comment('tipo_matricula, ordinaria,extraordinaria,especial');
-            $table->string('number')->comment('numero de folio de la matricula');
+                  ->comment('Estudiantes a ser matriculados reistra el id_persona_participante'); 
+            $table->foreignId('status_id')->constrained('app.catalogues')
+                  ->comment('Estado de la matricula (inscrito, matriculado, anulado, desertor)'); 
+            $table->foreignId('type_id')->constrained('app.catalogues')
+                  ->comment('tipo_matricula, ordinaria,extraordinaria,especial'); 
+            $table->string('number')->comment('numero de folio de la matricula'); 
+            $table->foreignId('planification_id')->constrained('cecy.detail_planifications')
+                  ->comment('cada maticula esta relacionada a un detalle de la planificación del curso'); 
             $table->softDeletes();
             $table->timestamps();
         });
