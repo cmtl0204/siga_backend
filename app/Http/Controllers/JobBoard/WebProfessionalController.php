92f3cd6b9644232666c62210cbaeafba038e6da6
OsorioCarlos <carlosalexander.2001@hotmail.com>
2021-06-11 18:54:11 -0500

    Fixed bugs

--- app/Http/Controllers/JobBoard/WebProfessionalController.php
+++ app/Http/Controllers/JobBoard/WebProfessionalController.php
@@ -111,24 +111,23 @@ class WebProfessionalController extends Controller
     function applyProfessional(Request $request)
     {
         $professional = Professional::find($request->input('professional_id'));
-        // // $company = User::find($request->input('user_id'))->company();
-        // $company = $request->user();
+        $company = User::find($request->user()->id)->company();
 
-        // // if (!$company) {
-        // //     return response()->json([
-        // //         'data' => null,
-        // //         'msg' => [
-        // //             'summary' => 'success',
-        // //             'detail' => '',
-        // //             'code' => '404'
-        // //         ]
-        // //     ], 404);
-        // // }
+        if (!$company) {
+            return response()->json([
+                'data' => null,
+                'msg' => [
+                    'summary' => 'error',
+                    'detail' => '',
+                    'code' => '404'
+                ]
+            ], 404);
+        }
         
-        // $company->professionals()->attach($professional->id);
+        $company->professionals()->attach($professional->id);
 
         return response()->json([
-            'data' => $request->user(),
+            'data' => null,
             'msg' => [
                 'summary' => 'success',
                 'detail' => 'Profesional contactado con éxito',
