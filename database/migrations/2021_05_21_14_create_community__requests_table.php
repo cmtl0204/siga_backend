dfeeb53803d6f43e7d2c4a0b33f45739cbb181d1
NancyMorales07 <nfd.morales@yavirac.edu.ec>
2021-06-18 22:39:11 -0500

    correcion de nobre de la tabla y caampos

--- database/migrations/2021_05_21_14_create_community__requests_table.php
+++ database/migrations/2021_05_21_14_create_community__requests_table.php
@@ -14,14 +14,14 @@ class CreateCommunityRequestsTable extends Migration
 	 
     public function up()
     {
-        Schema::connection('pgsql-community')->create('agreement_clauses', function (Blueprint $table) {
+        Schema::connection('pgsql-community')->create('requests', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->comment('FK de user')->connstrained('authentication.users'); // datos principales de estudiantes			
             $table->date('date_request')->nullable()->comment('fecha que el estudiante realiza la solicitud. ');
 			$table->string('status')->nullable()->comment('estado en la que se encuetra la solicitud.'); //Guarda el nombre del encabezado de la clausula.
 			$table->text('observation')->nullable()->comment('contenido de la clusula del convenio.'); // texto que describe la clausula del convenio.
 			$table->text('academic_period')->nullable()->comment('preriodo academico en el que hace la solicitud.'); 
-			$table->text('nivel')->nullable()->comment('nivel que se encuentra el estudiante.'); 
+			$table->text('level')->nullable()->comment('nivel que se encuentra el estudiante.'); 
 			
 			$table->softDeletes();
 			$table->timestamps();
