a406b31d43f0846021b180aa917565ffa32f0375
DanielaCisneros <ndc.tobar@yavirac.edu.ec>
2021-07-01 11:20:44 -0500

    probando git

--- app/Models/Authentication/User.php
+++ app/Models/Authentication/User.php
@@ -197,11 +197,7 @@ class User extends Authenticatable implements Auditable, MustVerifyEmail
     // Accessors
     function getFullNameAttribute()
     {
-<<<<<<< HEAD
-        return "{$this->attributes['first_name']} {$this->attributes['second_name']} " .
-=======
         return "{$this->attributes['names']}" .
->>>>>>> mod_3.2_authentication
             "{$this->attributes['first_lastname']} {$this->attributes['second_lastname']}";
     }
 
