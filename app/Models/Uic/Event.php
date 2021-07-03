b385c9078195a13c1867e7c59eaf244d121702c1
Luis Ortiz <mluis5144@gmail.com>
2021-07-02 20:08:48 -0500

    arreglo modelos

--- app/Models/Uic/Event.php
+++ app/Models/Uic/Event.php
@@ -9,43 +9,35 @@ use OwenIt\Auditing\Contracts\Auditable;
 use OwenIt\Auditing\Auditable as Auditing;
 
 // Application
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Dyrynda\Database\Support\CascadeSoftDeletes;
-
+/**
+ * @property BigInteger id
+ * @property string name
+ * @property string description
+ */
 class Event extends Model implements Auditable
 {
     use HasFactory;
     use Auditing;
-    use SoftDeletes;
-
-
-    protected static $instance;
+    use CascadeSoftDeletes;
 
     protected $connection = 'pgsql-uic';
     protected $table = 'uic.events';
+    //hacer despues
+    protected $with = [];
 
     protected $fillable = [
-
-        'id',
         'name',
         'description'
     ];
+
+    protected $cascadeDeletes = ['EventPlannings'];
 
     protected $casts = [
         'deleted_at' => 'date:Y-m-d h:m:s',
         'created_at' => 'date:Y-m-d h:m:s',
         'updated_at' => 'date:Y-m-d h:m:s',
     ];
-
-    public static function getInstance($id)
-    {
-        if (is_null(static::$instance)) {
-            static::$instance = new static;
-        }
-        static::$instance->id = $id;
-        return static::$instance;
-    }
-
 
     public function eventPlannings()
     {
